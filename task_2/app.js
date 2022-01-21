34a70d9d4df7a7f2f0386f96dc0e955876762dde
lexSerg <serginko21@gmail.com>
2022-01-21 14:20:32 +0200

    add register and login pages

--- task_2/app.js
+++ task_2/app.js
@@ -6,7 +6,7 @@ const path = require('path');
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
 app.use(express.static(path.join(process.cwd(), 'views')));
-app.engine('.hbs', hbsEngine({defaultLayout : false}));
+app.engine('.hbs', hbsEngine({defaultLayout : false, layoutsDir : process.cwd() + 'views/css'}));
 app.set('view engine', '.hbs');
 app.set('views', path.join(process.cwd(), 'views', 'tempaltes'));
 
@@ -16,4 +16,13 @@ app.listen(5000, () => {
 
 app.get('/', (req, res) => {
     res.render('main')
+})
+app.get('/login', (req, res) => {
+    res.render('login')
+});
+app.get('/register', (req, res) => {
+    res.render('register')
+})
+app.post('/login', (req, res) => {
+    console.log(req.body);
 })
